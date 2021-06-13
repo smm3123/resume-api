2887d6707626b05ff91e55d1494dd6cca483ce65
smm3123 <syedmahdi3123@gmail.com>
2021-06-12 21:25:45 -0500

    Fix linter issue

--- src/resume_helper.py
+++ src/resume_helper.py
@@ -1,16 +1,16 @@
-from src.resume import *
+import src.resume as resume_classes
 from typing import List
 
 
-def get_profiles() -> List[Profile]:
-    linkedin = Profile("LinkedIn", "Syed Mahdi", "www.linkedin.com/in/smmahdi/")
-    github = Profile("GitHub", "smm3123", "www.github.com/smm3123")
+def get_profiles() -> List[resume_classes.Profile]:
+    linkedin = resume_classes.Profile("LinkedIn", "Syed Mahdi", "www.linkedin.com/in/smmahdi/")
+    github = resume_classes.Profile("GitHub", "smm3123", "www.github.com/smm3123")
     profiles = [linkedin, github]
     return profiles
 
 
-def get_basics() -> Basics:
-    basics = Basics()
+def get_basics() -> resume_classes.Basics:
+    basics = resume_classes.Basics()
     basics.name = "Syed Mahdi"
     basics.label = "Software Engineer"
     basics.email = "syedmahdi3123@gmail.com"
@@ -19,7 +19,7 @@ def get_basics() -> Basics:
     return basics
 
 
-def get_resume() -> Resume:
-    resume = Resume()
+def get_resume() -> resume_classes.Resume:
+    resume = resume_classes.Resume()
     resume.basics = get_basics()
     return resume
